7157f58fe9e1ff59ca2406cc82f056e5096353c4
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-02-15 23:25:10 +0600

    even odd solving done

---
+++ even_odd.js
@@ -0,0 +1,18 @@
+let n = 0;
+
+const evenOdd = (n) => {
+  if (n < 0) {
+    console.log("Number is negative");
+    return;
+  }
+
+  if (n % 2 === 0) {
+    console.log("Even");
+    return;
+  }
+  if (n % 2 === 1) {
+    console.log("Odd");
+    return;
+  }
+};
+evenOdd(n);
---
+++ solution.js
@@ -0,0 +1,19 @@
+let n=2;
+
+
+const evenOdd=n=>{
+    if(n<=0){
+        console.log("Number is negative")
+        return
+    }
+    
+    if(n%2===0){
+        console.log(Even)
+        return
+    }
+    if(n%2===1){
+        console.log(Odd)
+        return
+    }
+}
+
